accde113cc29869a861f4b20d5c3297743dc47f2
nalden141 <noor15511551@gmail.com>
2023-08-11 11:41:10 +0200

    README.md second

--- app.py
+++ app.py
@@ -77,6 +77,9 @@ if __name__ == "__main__":
 
         predictions = lr.predict(train_x)
         signature = infer_signature(train_x, predictions)
+        # for remote server
+        remoteServer='https://dagshub.com/nalden141/MLflowExperiments.mlflow'
+        mlflow.set_tracking_uri(remoteServer)
 
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
